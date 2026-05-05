95883a3e742d101104ac0e6427fa0964584ac5ca
Jake Helfert <jake@helfert.us>
2026-05-05 07:10:53 -0600

    Fixed some issues with the x64 glibc toolchain file

--- prebuilts/cmake/x86_64-linux-gnu.cmake
+++ prebuilts/cmake/x86_64-linux-gnu.cmake
@@ -7,13 +7,19 @@ set(CMAKE_C_COMPILER   "${TOOLCHAIN_ROOT}/bin/clang")
 set(CMAKE_CXX_COMPILER "${TOOLCHAIN_ROOT}/bin/clang++")
 set(CMAKE_ASM_COMPILER "${TOOLCHAIN_ROOT}/bin/clang")
 
-set(CMAKE_C_COMPILER_TARGET   x86_64-linux-gnu)
-set(CMAKE_CXX_COMPILER_TARGET x86_64-linux-gnu)
-set(CMAKE_ASM_COMPILER_TARGET x86_64-linux-gnu)
+set(CMAKE_C_COMPILER_TARGET   x86_64-unknown-linux-gnu)
+set(CMAKE_CXX_COMPILER_TARGET x86_64-unknown-linux-gnu)
+set(CMAKE_ASM_COMPILER_TARGET x86_64-unknown-linux-gnu)
 
 if(NOT DEFINED CMAKE_SYSROOT)
     set(CMAKE_SYSROOT "${TOOLCHAIN_ROOT}/sysroots/x64")
 endif()
+
+set(CMAKE_CXX_FLAGS_INIT "--stdlib=libc++")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT    "-fuse-ld=lld --rtlib=compiler-rt --stdlib=libc++ -unwindlib=libunwind")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld --rtlib=compiler-rt --stdlib=libc++ -unwindlib=libunwind")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld --rtlib=compiler-rt --stdlib=libc++ -unwindlib=libunwind")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
